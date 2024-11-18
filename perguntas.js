23e6121e55ae018c272972880fa4fe0997a394af
Isadora <137515981+iisadorac@users.noreply.github.com>
2024-11-18 18:54:50 +0000

    18/11/2024

--- perguntas.js
+++ perguntas.js
@@ -1,33 +1,44 @@
 criaCartao(
     '1',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Quem foi o imperador durante o Segundo Reinado no Brasil?',
+    ' O imperador foi Dom Pedro II. Ele assumiu o trono em 1831, após a abdicação de seu pai, Dom Pedro I, e reinou até a proclamação da República em 1889.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é JavaScript?',
-    'O JavaScript é uma linguagem de programação.'
+    '2',
+    'Quais foram as principais características da política interna durante o Segundo Reinado?',
+    'O governo foi estável, com foco na economia do café, manutenção da escravidão e um regime monárquico, onde o imperador tinha grande poder, mas também existia um parlamento.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é CSS?',
-    'O CSS é uma lingugem de estilização!'
+    '3',
+    'O que foi a Abolição da Escravatura no Brasil?',
+    'A Abolição aconteceu em 13 de maio de 1888, com a Lei Áurea, assinada pela princesa Isabel, que libertou os escravizados no Brasil.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é HTML?',
-    'O HTML é uma lingugem de estruturação e semântica do conteúdo.'
+    '4',
+    'Quais crises internas o Brasil enfrentou durante o Segundo Reinado?',
+    'O Brasil enfrentou revoltas regionais (como a Farroupilha e a Cabanagem), a Guerra do Paraguai e descontentamento de várias camadas da sociedade.'
 )
 criaCartao(
-    'Programação',
-    'O que é VSCode?',
-    'O Visual Studio Code é um editor de código com diversas integrações.s'
+    '5',
+    'O que foi a Guerra do Paraguai e qual foi seu impacto?',
+    'Foi uma guerra entre o Brasil, Argentina e Uruguai contra o Paraguai (1864-1870). O Brasil perdeu muitas vidas e dinheiro, e a guerra teve grande impacto negativo no país.'
 )
 criaCartao(
-    'Programação',
-    'O que é GitHub?',
-    'O GitHub é uma plataforma de desenvolvimento colaborativo que aloja projetos na nuvem.'
+    '6',
+    ' Como a economia do Brasil mudou durante o Segundo Reinado?',
+    'A economia cresceu principalmente com o café, que se tornou o principal produto de exportação. Também houve construção de ferrovias e imigração de europeus para o Brasil.'
 )
+criaCartao(
+    '7',
+    'Qual foi a relação do Brasil com outros países durante o Segundo Reinado?',
+    'O Brasil manteve boas relações com países da Europa, especialmente a Inglaterra, e foi ativo no comércio internacional, principalmente com o café.'
+)
+criaCartao(
+    '8',
+    'Como o Segundo Reinado terminou?',
+    'O Segundo Reinado acabou em 1889, com a Proclamação da República, quando militares depuseram o imperador Dom Pedro II e instauraram o regime republicano.'
+)
+
